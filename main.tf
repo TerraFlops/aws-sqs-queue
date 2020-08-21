b8cac886b4aa5648180a0294888d5bc043dacb63
Damian Sloane <damian@capacitorlabs.com>
2020-08-21 14:27:13 +1000

    Use policy attribute

--- main.tf
+++ main.tf
@@ -7,11 +7,5 @@ resource "aws_sqs_queue" "queue" {
   kms_data_key_reuse_period_seconds = var.kms_data_key_reuse_period_seconds
   kms_master_key_id = var.kms_master_key_id
   tags = var.tags
-}
-
-# Optionally attach policy document to queue
-resource "aws_sqs_queue_policy" "queue" {
-  count = var.policy_document != null ? 1 : 0
   policy = var.policy_document
-  queue_url = aws_sqs_queue.queue.id
 }
