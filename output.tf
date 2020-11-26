295c7e36e7e5c5a0ae9371985516cc7e05987aee
Damian Sloane <damian@capacitorlabs.com>
2020-11-26 15:47:01 +1100

    Work in progress

--- output.tf
+++ output.tf
@@ -9,3 +9,11 @@ output "url" {
 output "name" {
   value = aws_sqs_queue.queue.name
 }
+
+output "sns_topic_message_age_alarm" {
+  value = aws_sns_topic.message_age_alarm.name
+}
+
+output "sns_topic_message_count_alarm" {
+  value = aws_sns_topic.message_count_alarm.name
+}
