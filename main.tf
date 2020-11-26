a3529d2da6f93fe50e83f6d6d7980a5e178d2d68
Damian Sloane <damian@capacitorlabs.com>
2020-11-26 11:35:38 +1100

    Work in progress

--- main.tf
+++ main.tf
@@ -1,3 +1,31 @@
+locals {
+  name_snake = join("", [for element in split("-", lower(var.name)) : title(element)])
+}
+
+variable "evaluation_periods" {
+  type = number
+  description = "Number of evaluation periods required before alarming"
+  default = 5
+}
+
+variable "period" {
+  type = number
+  description = "Length of evaluatin period (seconds)"
+  default = 60
+}
+
+variable "statistic" {
+  type = string
+  description = "Evaluation statistic (e.g. Maximum, Average)"
+  default = "Average"
+}
+
+variable "threshold" {
+  type = string
+  description = "Number of visible queue messages before alarm triggers"
+  default = 5000
+}
+
 # Create queue
 resource "aws_sqs_queue" "queue" {
   name = var.name
@@ -8,4 +36,21 @@ resource "aws_sqs_queue" "queue" {
   kms_master_key_id = var.kms_master_key_id
   tags = var.tags
   policy = var.policy_document
+}
+
+# Create CloudWatch alarm
+resource "aws_cloudwatch_metric_alarm" "queue_message_visible_alarm" {
+  alarm_name = "${local.name_snake}MessageVisible"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods = var.evaluation_periods
+  metric_name = "ApproximateNumberOfMessagesVisible"
+  namespace = "AWS/SQS"
+  period = var.period
+  statistic = var.statistic
+  threshold = var.threshold
+  treat_missing_data = "notBreaching"
+
+  dimensions = {
+    QueueName = aws_sqs_queue.queue.name
+  }
 }
