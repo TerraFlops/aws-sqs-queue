5d3ada5771f390b1147875ce821ba9f7a9e79b13
Damian Sloane <damian@capacitorlabs.com>
2020-11-30 10:45:50 +1100

    Work in progress

--- output.tf
+++ output.tf
@@ -9,16 +9,3 @@ output "url" {
 output "name" {
   value = aws_sqs_queue.queue.name
 }
-
-output "sns_topic_message_age_alarm" {
-  value = aws_sns_topic.message_age_alarm.name
-}
-
-output "sns_topic_message_count_alarm" {
-  value = aws_sns_topic.message_count_alarm.name
-}
-
-output "opsgenie_api_key" {
-  description = "If a list of Opsgenie responders was specified, this will be the API key of the Opsgenie integration"
-  value = var.opsgenie_integration_name != null && (length(var.opsgenie_responders_users) > 0 || length(var.opsgenie_responders_teams) > 0) ? opsgenie_api_integration.opsgenie_integration[0].api_key : null
-}
